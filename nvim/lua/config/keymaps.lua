468a3d5f01204463d3340e1b6ec5134ec4d9865e
Gary <gary@gary-dev.com>
2024-08-14 11:35:07 +1000

    Tabspace conversion function.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,4 +4,26 @@
 
 local map = vim.keymap.set
 
+-- Undo
 map("n", "U", "<c-r>", { noremap = true, silent = true })
+
+---@param current number
+---@param transformed number
+local function convertTabSpaces(current, transformed)
+  local o = vim.o
+
+  o.expandtab = false
+  o.tabstop = current
+  vim.cmd("%retab!")
+
+  o.tabstop = transformed
+  o.expandtab = true
+  vim.cmd("retab")
+end
+
+map("n", "<leader>cF24",
+  function()
+    convertTabSpaces(2, 4)
+  end,
+  { desc = "Converts 2 tabstop to 4 tabstop" }
+)
